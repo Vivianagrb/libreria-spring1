0c32c454c86c78dcfef6d212835473afb804ceae
juanc <vivianagrb@hotmail.com>
2025-06-14 22:28:29 -0500

    Mi primer comentario en git: Creacion de clases con patron de inyeccion de dependencia y conexion a bdd.

--- src/main/java/com/distribuida/service/LibroService.java
+++ src/main/java/com/distribuida/service/LibroService.java
@@ -1,14 +1,13 @@
 package com.distribuida.service;
 
 import com.distribuida.model.Libro;
+
 import java.util.List;
 
 public interface LibroService {
-    public List<Libro> findAll();
-    public Libro findOne(int id_libro);
-    public  Libro save(Libro libro);
-    public  Libro update(int id_libro, Libro libro);
-    public void delete(int id_libro);
-
-
+    List<Libro> findAll();
+    Libro findOne(int id_libro);
+    Libro save(Libro libro);
+    Libro update(int id_libro, Libro libro);
+    void delete(int id_libro);
 }
